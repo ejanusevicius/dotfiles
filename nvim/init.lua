cb93dfe8282796104cae236646eb2653d1738c07
ejanusevicius <ejanusevicius1@gmail.com>
2024-04-28 19:57:28 +0100

    feat: added in-terminal lazygit, updated telescope config

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,12 +20,6 @@ require("lazy").setup("plugins")
 
 vim.cmd[[colorscheme tokyonight]]
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 
