00665ade6a4bcbcb40ea51453071ee68d69a86c9
ejanusevicius <ejanusevicius1@gmail.com>
2024-05-27 09:38:55 +0100

    feat: more plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,6 +18,10 @@ require("config.options").setup()
 -- Loading "lazy.nvim" only after config
 require("lazy").setup("plugins")
 
+require("catppuccin").setup({
+  flavour = "mocha",
+})
+
 vim.diagnostic.config({
   virtual_text = true,
 })
@@ -69,9 +73,32 @@ require("gitsigns").setup({
   },
 })
 
+require("nvim-ts-autotag").setup({
+  opts = {
+    -- Defaults
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = false, -- Auto close on trailing </
+  },
+  -- Also override individual filetype configs, these take priority.
+  -- Empty by default, useful if one of the "opts" global settings
+  -- doesn't work well in a specific filetype
+  per_filetype = {
+    ["html"] = {
+      enable_close = false,
+    },
+  },
+})
+
 -- i/o
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "save file" })
 vim.keymap.set("n", "<C-q>", "<cmd>q<CR>", { desc = "exit nvim" })
+
+-- text editing
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
 
 -- nvimtree
 vim.keymap.set("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
@@ -80,6 +107,10 @@ vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree fo
 -- whichkey
 vim.keymap.set({ "n", "v" }, "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
 
+-- buffers changes
+vim.keymap.set("n", "<leader>]", "<cmd>bnext <CR>", { desc = "next buffer" })
+vim.keymap.set("n", "<leader>[", "<cmd>bprev <CR>", { desc = "prev buffer" })
+
 -- global lsp mappings
 vim.keymap.set("n", "<leader>lf", vim.diagnostic.open_float, { desc = "lsp floating diagnostics" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "lsp prev diagnostic" })
@@ -87,4 +118,4 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "lsp next diagnosti
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "lsp diagnostic loclist" })
 
 vim.o.termguicolors = true
-vim.cmd([[colorscheme tokyonight]])
+vim.cmd([[colorscheme catppuccin]])
